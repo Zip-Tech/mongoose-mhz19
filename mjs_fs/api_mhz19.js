34a6c86011dd2a5caa5fe0b14b558540ac25bad2
john.dela.rosa <jrddelarosa@gmail.com>
2020-11-27 10:24:05 +0800

    app log test

--- mjs_fs/api_mhz19.js
+++ mjs_fs/api_mhz19.js
@@ -3,6 +3,14 @@
 
 load('api_math.js');
 
+// mySerial.begin(BAUDRATE);                               // Uno example: Begin Stream with MHZ19 baudrate
+
+// mySerial.begin(BAUDRATE, SERIAL_8N1, RX_PIN, TX_PIN);   // ESP32 Example
+
+// myMHZ19.begin(mySerial);                // *Important, Pass your Stream reference here
+
+// myMHZ19.autoCalibration(false);         // Turn auto calibration OFF
+
 let MHZ19 = {
     // Error codes
     RES_FAIL: -100.0,
@@ -15,18 +23,18 @@ let MHZ19 = {
     // // ## **`MHZ19.create()`**
     // // Create a MHZ19 instance.
     // // Return value: an object with the methods described below.
-    // create: function(serial) {
-    //     let obj = Object.create(MHZ19._proto);
-    //     // Initialize MHZ19 library.
-    //     // Return value: handle opaque pointer.
-    //     obj.mhz = MHZ19._c();
-    //     let b = MHZ19._begin(obj.mhz);
-    //     if (b === 0) {
-    //     // Can't find a sensor
-    //         return undefined;
-    //     }
-    //      return obj;
-    // },
+    create: function(serial) {
+        let obj = Object.create(MHZ19._proto);
+        // Initialize MHZ19 library.
+        // Return value: handle opaque pointer.
+        obj.mhz = MHZ19._c();
+        // let b = MHZ19._begin(obj.mhz, serial);
+        // if (b === 0) {
+        // // Can't find a sensor
+        //     return undefined;
+        // }
+         return obj;
+    },
 
     _proto: {
         // ## **`myMHZ.close()`**
